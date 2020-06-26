ceb593eb1daee2b014a5e4ba3852b3d7a5aa9528
“zhangmin <531474715@qq.com>
2020-06-26 23:55:56 +0800

    超市程序

--- HelloJava.java
+++
@@ -1,5 +0,0 @@
-public class HelloJava {
-    public static void main(String[] args) {
-        System.out.print("开始学习java，每天都要有所进步！！加油");
-    }
-}
--- 面向对象编程/打造一个小超市/code/src/com/bang/persion/Customer.java
+++
@@ -1,7 +0,0 @@
-package com.bang.persion;
-
-public class Customer {
-    public String name;
-    public double money;
-    public boolean isDrivingCar;
-}
